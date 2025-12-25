e465f259a4510e8176e94179b2f2883229e63107
gayatrigaik1101-ui <gayatri.gaik1101@gmail.com>
2025-12-25 20:35:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,9 +54,9 @@ if user_input:
     result = query({"inputs": prompt})
 
    if isinstance(result, list) and "generated_text" in result[0]:
-    reply = result[0]["generated_text"]
-elif isinstance(result, dict) and result.get("error"):
-    reply = "Main thodi der mein ready ho jaungi 😊 please 20–30 seconds baad phir poochna."
-else:
-    reply = "Ek second… main response prepare kar rahi hoon 😊 thodi der baad phir try karte hain."
+       reply = result[0]["generated_text"]
+   elif isinstance(result, dict) and result.get("error"):
+       reply = "Main thodi der mein ready ho jaungi 😊 please 20–30 seconds baad phir poochna."
+   else:
+       reply = "Ek second… main response prepare kar rahi hoon 😊 thodi der baad phir try karte hain."
 
